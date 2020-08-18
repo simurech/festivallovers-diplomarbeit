82b7910f7fe5ef37ac301d677d37d5f6c3da934b
SImon <simurech@gmail.com>
2020-08-18 12:37:30 +0200

    Filestruktur aller Seiten erstellt, Inhalte und Funnktionen nicht ausstehend. REST-Anbindung für Festival-Übersicht erstellt und grundlegend konfiguriert.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // Test ob JS überhaupt geladen wird.
-function testFunktion () {
+function testFunktion() {
 	console.warn('JS wurde erfolgreich geladen');
 }
 testFunktion();
@@ -7,7 +7,7 @@ testFunktion();
 
 //! Standardfunktionen
 // Link öffnen (selbes Fenster)
-function linkOeffnen (link) {
+function linkOeffnen(link) {
 	window.location.href=link;
 };
 
@@ -15,7 +15,7 @@ function linkOeffnen (link) {
 
 //! Home – Tablet & Desktop: Klick auf "Mehr Genres" bleindet weitere Genres ein.
 // Funktion
-function zeigMehrGenres () {
+function zeigMehrGenres() {
 	let genreLabels = document.querySelectorAll('.filter__genres-item-label');
 	for (i = 0; i < genreLabels.length; i++ ){
 		genreLabels[i].classList.remove('notvisible');
@@ -31,7 +31,7 @@ document.querySelector('.filter__genres-more').addEventListener('click', zeigMeh
 
 //! Home – Mobile: Klick auf "Filter anzeigen" bleindet Genres ein.
 // Funktion
-function zeigGenres () {
+function zeigGenres() {
 	let genreList = document.querySelectorAll('.filter__genres-list');
 	genreList[0].style.display = 'flex';
 	let genreAction = document.querySelectorAll('.filter__action');
@@ -46,7 +46,7 @@ document.querySelector('.filter__toggle').addEventListener('click', zeigGenres);
 
 //! Home: Klick auf "Filter zurücksetzen" unchecked die Genre-Filter
 // Funktion
-function filterZuruecksetzen () {
+function filterZuruecksetzen() {
 	let genreCheckbox = document.querySelectorAll('.filter__genres-item-checkbox');
 	for (i = 0; i < genreCheckbox.length; i++ ){
 		genreCheckbox[i].checked = false;
@@ -59,7 +59,7 @@ document.querySelector('.zuruecksetzen').addEventListener('click', filterZurueck
 
 //! Home: Klick auf "Ergebnis anzeigen" leitet auf Übersichts-Seite mit allen Festivals 
 // Funktion
-function ergebnisseAnzeigen () {
+function ergebnisseAnzeigen() {
 	let genreCheckbox = document.querySelectorAll('.filter__genres-item-checkbox');
 	let genres = []
 	for (i = 0; i < genreCheckbox.length; i++ ){
@@ -69,11 +69,122 @@ function ergebnisseAnzeigen () {
 		};
 	};
 	console.log(genres);
-	let link = '/festival-uebersicht/';
+	let link = '/festival-uebersicht';
 	for (i = 0; i < genres.length; i++ ){
 		link = link + '?genre=' + genres[i];
 		};
-	linkOeffnen (link);
+	linkOeffnen(link);
 };
 // Event Listener
-document.querySelector('.ergebnis').addEventListener('click', ergebnisseAnzeigen);
+//? document.querySelector('.ergebnis').addEventListener('click', ergebnisseAnzeigen);
+
+
+
+//! Übersicht: Lade alle Festivals
+const restUrl = 'http://192.168.1.10/rest' //! URL ZU SERVER
+// Event-Daten laden
+function eventsLaden() {
+		fetch(restUrl+'/events/')
+				.then(response =>{
+					return response.json()
+				})
+				.then(events => {
+					const ul = document.querySelector('#events');
+					events.forEach(event => {
+						const li = document.createElement ('li');
+						const locationId = event.location_id;
+						const locationName = getLocationNameById(locationId);
+						li.setAttribute('class', event.genre_id + ' festivals__item ' + event.id);
+						li.innerHTML = '<div>'+
+							'<p> ID= '+event.id+'</p>'+
+							'<h3>title= '+event.title+'</h3>'+
+							'<h4>subtitle= '+event.subtitle+'</h4>'+
+							'<p>desc_title= '+event.desc_title+'</p>'+
+							'<p>desc_lead= '+event.desc_lead+'</p>'+
+							'<p>desc_text= '+event.desc_text+'</p>'+
+							'<p>genre_id= '+event.genre_id+'</p>'+
+							'<p>location_id= '+event.location_id+'</p>'+
+							'<p id="'+event.location_id+'">Locationname= NAME:'+locationName+'</p>'+
+							'<p>date_start= '+event.date_start+'</p>'+
+							'<p>date_end= '+event.date_end+'</p>'+
+							'</div>';
+						ul.appendChild(li);
+					});
+				});
+		};
+// Funktion sofort ausführen
+eventsLaden();
+
+//! LocationID in Name umwandeln - DEFEKT
+// Funktion: Location mit spezifischer ID abfragen -> Namen zurückgeben
+function getLocationNameById(id){
+	fetch(restUrl+'/locations/'+id)
+		.then(response =>{
+			return response.json();
+		})
+		.then(json => {
+			const title = json.title;
+			return title;
+		})
+		.then(console.log('Fertig?'+this.title));
+};
+
+
+// Location ID in Text umwandeln
+function rewriteLocationIdToText(id){
+	const requestId = id;
+	if (requestId === '1') {
+			return 'Bern';
+		}else if (requestId === '2'){
+			return 'Bülach';
+		}else if (requestId === '3'){
+			return 'Bülach';
+		}else if (requestId === '4'){
+			return 'Bülach';
+		}else if (requestId === '5'){
+			return 'Bülach';
+		}else if (requestId === '6'){
+			return 'Bülach';
+		}else if (requestId === '7'){
+			return 'Bülach';
+		}else if (requestId === '8'){
+			return 'Bülach';
+		}else if (requestId === '9'){
+			return 'Bülach';
+		}else if (requestId === '10'){
+			return 'Bülach';
+		}else if (requestId === '11'){
+			return 'Bülach';
+		}else if (requestId === '12'){
+			return 'Bülach';
+		}else if (requestId === '13'){
+			return 'Bülach';
+		}else if (requestId === '14'){
+			return 'Bülach';
+		}else if (requestId === '15'){
+			return 'Bülach';
+		}else if (requestId === '16'){
+			return 'Bülach';
+		}else if (requestId === '17'){
+			return 'Bülach';
+		}else if (requestId === '18'){
+			return 'Bülach';
+		}else if (requestId === '19'){
+			return 'Bülach';
+		}else if (requestId === '20'){
+			return 'Bülach';
+		}else if (requestId === '21'){
+			return 'Bülach';
+		}else if (requestId === '22'){
+			return 'Bülach';
+		}else if (requestId === '2'){
+			return 'Bülach';
+		}else if (requestId === '2'){
+			return 'Bülach';
+		}
+		else {
+			return 'Standort wurde nicht gefunden';
+	  };
+}
+// Funktion sofort ausführen
+rewriteLocationIdToText()
